67ec9297cf6494023ad9cae33709b47c7761bf8c
bannovdaniil <baddan@mail.ru>
2023-09-17 10:31:23 +0300

    update SQL

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -7,32 +7,33 @@ DROP TABLE IF EXISTS phonenumbers CASCADE;
 CREATE TABLE IF NOT EXISTS roles
 (
     role_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name    VARCHAR(255) NOT NULL
+    role_name    VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS departments
 (
     department_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name          VARCHAR(255) NOT NULL
+    department_name          VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS users
 (
-    user_uuid uuid DEFAULT gen_random_uuid() PRIMARY KEY,
-    firstName VARCHAR(255) NOT NULL,
-    lastName  VARCHAR(255) NOT NULL,
+    user_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_firstName VARCHAR(255) NOT NULL,
+    user_lastName  VARCHAR(255) NOT NULL,
     role_id   BIGINT REFERENCES roles (role_id)
 );
 
 CREATE TABLE IF NOT EXISTS users_departments
 (
-    user_uuid     uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    users_departments_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id     BIGINT REFERENCES users (user_id),
     department_id BIGINT REFERENCES departments (department_id)
 );
 
 CREATE TABLE IF NOT EXISTS phonenumbers
 (
     phonenumber_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    number         VARCHAR(255) NOT NULL,
-    user_id        uuid REFERENCES users (user_uuid)
+    phonenumber_number         VARCHAR(255) NOT NULL,
+    user_id        BIGINT REFERENCES users (user_id)
 );
