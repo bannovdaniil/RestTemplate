a311b4217420192405f16ce32b7f2ef7b86ab47c
bannovdaniil <baddan@mail.ru>
2023-10-03 23:36:35 +0300

    add data base initializer

---
+++ src/main/java/org/example/servlet/DbServlet.java
@@ -0,0 +1,37 @@
+package org.example.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.db.ConnectionManager;
+import org.example.db.ConnectionManagerImpl;
+import org.example.util.InitSqlScheme;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(urlPatterns = {"/db"})
+public class DbServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        StringBuilder sb = new StringBuilder("Create data base:\n\n");
+
+        ConnectionManager connectionManager = ConnectionManagerImpl.getInstance();
+        sb.append("Create schema\n");
+        InitSqlScheme.initSqlScheme(connectionManager);
+        sb.append("Create data\n");
+        InitSqlScheme.initSqlData(connectionManager);
+
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("UTF-8");
+
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write(sb.toString());
+        printWriter.flush();
+    }
+
+
+}
